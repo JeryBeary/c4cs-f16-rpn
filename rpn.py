e6dab359036ac8555cb44b30996bd167ad2ac8bd
Jerry Meng <ymeng@umich.edu>
2016-10-07 14:19:57 -0400

    sdadad

--- rpn.py
+++ rpn.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-def calculate(string):
-	pass
+def calculate(myarg1):
+	stack = list()
+	for token in myarg1.split():
+		if token == '+':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1 + arg2
+			stack.append(token)
+		else:
+			stack.append(int(token))
+		print(stack)
 
 def main():
 	while True:
